334666b25ce3f850b614eb0bb169686bed04956f
dylanjsa90 <dylanjsanders1@gmail.com>
2016-05-20 12:07:12 -0700

    methods and functions refactored

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var coffeeLocations = [];
+var pikePlace = new CampCoffee('Pike Place Market', 14, 34, 1.2, 0.34);
+var capitolHill = new CampCoffee('Capitol Hill', 12, 28, 3.2, 0.03);
+var seattlePublicLibrary = new CampCoffee('Seattle Public Library', 9, 45, 2.6, 0.02);
+var southLakeUnion = new CampCoffee('South Lake Union', 5, 18, 1.3, 0.04);
+var seaTacAirport = new CampCoffee('SeaTac Airport', 28, 44, 1.1, 0.41);
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm:', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm:'];
 var totals = [0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0, 0, 0];
 var total = 0;
@@ -73,65 +78,25 @@ CampCoffee.prototype.calcBaristaHours = function() {
   }
 };
 
-CampCoffee.prototype.renderBeanRow = function() {
-  var table = document.getElementById('beans-table');
+function renderRow(id, name, locationTotal, totalArray) {
+  var table = document.getElementById(id);
   var trElement = document.createElement('tr');
   var tdElement = document.createElement('td');
-  tdElement.textContent = this.locationName;
+  tdElement.textContent = name;
   trElement.appendChild(tdElement);
   tdElement = document.createElement('td');
-  tdElement.textContent = Math.ceil(this.totalBeans);
+  tdElement.textContent = Math.ceil(locationTotal);
   trElement.appendChild(tdElement);
-  total += Math.ceil(this.totalBeans);
+  total += Math.ceil(locationTotal);
   for (var i = 0; i < hours.length; i++) {
     tdElement = document.createElement('td');
-    tdElement.textContent = Math.ceil(this.totalBeansPerHour[i]);
-    totals[i] += Math.ceil(this.totalBeansPerHour[i]);
+    tdElement.textContent = Math.ceil(totalArray[i]);
+    totals[i] += Math.ceil(totalArray[i]);
     trElement.appendChild(tdElement);
   }
   table.appendChild(trElement);
 };
 
-CampCoffee.prototype.renderBaristaRow = function() {
-  var table = document.getElementById('baristas-table');
-  var trElement = document.createElement('tr');
-  var tdElement = document.createElement('td');
-  tdElement.textContent = this.locationName;
-  trElement.appendChild(tdElement);
-  tdElement = document.createElement('td');
-  tdElement.textContent = Math.ceil(this.totalBarristaHours);
-  trElement.appendChild(tdElement);
-  total += Math.ceil(this.totalBarristaHours);
-  for (var i = 0; i < hours.length; i++) {
-    tdElement = document.createElement('td');
-    tdElement.textContent = Math.ceil(this.barristaPerHour[i]);
-    totals[i] += Math.ceil(this.barristaPerHour[i]);
-    trElement.appendChild(tdElement);
-  }
-  table.appendChild(trElement);
-};
-var pikePlace = new CampCoffee('Pike Place Market', 14, 34, 1.2, 0.34);
-var capitolHill = new CampCoffee('Capitol Hill', 12, 28, 3.2, 0.03);
-var seattlePublicLibrary = new CampCoffee('Seattle Public Library', 9, 45, 2.6, 0.02);
-var southLakeUnion = new CampCoffee('South Lake Union', 5, 18, 1.3, 0.04);
-var seaTacAirport = new CampCoffee('SeaTac Airport', 28, 44, 1.1, 0.41);
-// Create Bean total table
-function renderAllBeans() {
-  tableHeadings('Daily Location ', 'beans-table');
-  for (var i = 0; i < coffeeLocations.length; i++) {
-    coffeeLocations[i].renderBeanRow();
-  }
-  totalRow('beans-table');
-}
-// Create barista total table
-function renderAllBarista() {
-  var table = document.getElementById('baristas-table');
-  tableHeadings('', 'baristas-table');
-  for (var i = 0; i < coffeeLocations.length; i++) {
-    coffeeLocations[i].renderBaristaRow();
-  }
-  totalRow('baristas-table');
-}
 // Create and append table header row for table with argument id
 function tableHeadings (title, id) {
   var table = document.getElementById(id);
@@ -165,6 +130,7 @@ function totalRow (id){
     trElement.appendChild(tdElement);
   }
   table.appendChild(trElement);
+  clearTotals();
 }
 function clearTotals() {
   total = 0;
@@ -195,11 +161,12 @@ function handleLocationSubmit(event) {
     if (locationName === coffeeLocations[i].locationName) {
       preExistingLocation = true;
       newLocationData = new CampCoffee(locationName, minCustomersHour, maxCustomersHour, cupsPerCustomer, packagedLbsPerCustomer);
-      coffeeLocations.splice(i, 1, newLocationData); // Replace repeat location with new instance for that location
+      coffeeLocations[i] = newLocationData;
       coffeeLocations = coffeeLocations.slice(0, -1); // Remove extra instance pushed to array from constructor
+      break;
+      // coffeeLocations.splice(i, 1, newLocationData); // Replace repeat location with new instance for that location
     }
   }
-
   // Create new location if repeated entry not found
   if (!preExistingLocation) {
     var newLocation = new CampCoffee(locationName, minCustomersHour, maxCustomersHour, cupsPerCustomer, packagedLbsPerCustomer);
@@ -220,13 +187,17 @@ function handleLocationSubmit(event) {
 }
 
 function renderAll() {
+  renderTable('beans-table', 'Daily Location ', 'totalBeans', 'totalBeansPerHour');
+  renderTable('baristas-table', '', 'totalBarristaHours', 'barristaPerHour');
+}
+
+function renderTable(id, heading,total, totalArray) {
+  tableHeadings(heading, id);
   for (var i = 0; i < coffeeLocations.length; i++) {
     calculateData(coffeeLocations[i]); // Fill instance data for all locations
+    renderRow(id, coffeeLocations[i].locationName, coffeeLocations[i][total], coffeeLocations[i][totalArray]); // Fill in all table rows
   }
-  renderAllBeans(); // Fill in all table rows
-  clearTotals(); // Reset total counters for total row
-  renderAllBarista();// Fill in table rows
-  clearTotals(); // Reset total counters
+  totalRow(id);
 }
 inputForm.addEventListener('submit', handleLocationSubmit);
 renderAll();
